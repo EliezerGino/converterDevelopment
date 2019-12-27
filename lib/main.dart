e47c57cc54475e9895ef23aa8791020b2e8565d5
Eliezer Castro <eliezergino@gmail.com>
2019-12-27 07:30:19 -0300

    Criando interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
-
 import "package:http/http.dart" as http;
 import 'dart:async';
 import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance";
 
+Future<Map> getData() async{
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+} 
+
 void main(List<String> args) async {
   runApp(
     MaterialApp(
-      home: Home()
+      home: Home(), 
+      theme: ThemeData(
+        hintColor: Colors.amber,
+        primaryColor: Colors.white
+      ),
     )
   );
 }
@@ -19,6 +27,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  double dolar;
+  double euro;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,7 +67,56 @@ class _HomeState extends State<Home> {
                   ),
                 );
               }else{
-                return Container(color: Colors.green,);
+                dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Icon(Icons.monetization_on, size: 150.0, color: Colors.amber),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Reais',
+                          labelStyle: TextStyle(color: Colors.amber),
+                          border: OutlineInputBorder(),
+                          prefixText: 'R\$ '
+                        ),
+                        style: TextStyle(
+                          color: Colors.amber, fontSize: 25,
+                        ),
+                      ),
+                      Divider(),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Dolars',
+                          labelStyle: TextStyle(color: Colors.amber),
+                          border: OutlineInputBorder(),
+                          prefixText: 'US\$ '
+                        ),
+                        style: TextStyle(
+                          color: Colors.amber, fontSize: 25,
+                        ),
+                      ),
+                      Divider(),
+
+                      
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Euros',
+                          labelStyle: TextStyle(color: Colors.amber),
+                          border: OutlineInputBorder(),
+                          prefixText: '€ '
+                        ),
+                        style: TextStyle(
+                          color: Colors.amber, fontSize: 25,
+                        ),
+                      )
+                    ],
+                  ),
+                );
+
               }
           }
         },
@@ -63,8 +124,3 @@ class _HomeState extends State<Home> {
     );
   }
 }
-
-Future<Map> getData() async{
-  http.Response response = await http.get(request);
-  return json.decode(response.body);
-} 
